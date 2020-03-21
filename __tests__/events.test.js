d6769430906eda4b02493e9bbaafdefee3705107
Dakota Druley <58957027+dakotadruley@users.noreply.github.com>
2020-03-21 08:08:14 -0700

    updated my event model tests but one is skipping still

--- __tests__/events.test.js
+++ __tests__/events.test.js
@@ -45,28 +45,16 @@ describe('event routes', () => {
   });
 
   it('creates an event', () => {
-    return Recipe.create({
-      name: 'cookies',
-      ingredients: [
-        { name: 'flour', amount: 1, measurement: 'cup' }
-      ],
-      directions: [
-        'preheat oven to 375',
-        'mix ingredients',
-        'put dough on cookie sheet',
-        'bake for 10 minutes'
-      ],
-    })
-      .then(recipe => {
-        return request(app)
-          .post('/api/v1/events')
-          .send({
-            recipeId: recipe.id,
-            dateOfEvent: Date.now(),
-            notes: 'It went well',
-            rating: 4
-          });
+    // updated 
+    return request(app)
+      .post('/api/v1/events')
+      .send({
+        recipeId: recipe._id,
+        dateOfEvent: Date.now(),
+        notes: 'It went well',
+        rating: 4
       })
+      
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
@@ -80,19 +68,6 @@ describe('event routes', () => {
   });
 
   it('gets all events', async() => {
-    const recipe = await Recipe.create({
-      name: 'cookies',
-      ingredients: [
-        { name: 'flour', amount: 1, measurement: 'cup' }
-      ],
-      directions: [
-        'preheat oven to 375',
-        'mix ingredients',
-        'put dough on cookie sheet',
-        'bake for 10 minutes'
-      ],
-    });
-
     const events = await Event.create([
       { recipeId: recipe.id, dateOfEvent: Date.now(), rating: 3 },
       { recipeId: recipe.id, dateOfEvent: Date.now(), rating: 2 },
@@ -110,84 +85,31 @@ describe('event routes', () => {
   });
 
   it('gets an event by id', async() => {
-    const recipe = await Recipe.create({
-      name: 'cookies',
-      ingredients: [
-        { name: 'flour', amount: 1, measurement: 'cup' }
-      ],
-      directions: [
-        'preheat oven to 375',
-        'mix ingredients',
-        'put dough on cookie sheet',
-        'bake for 10 minutes'
-      ],
-    });
-
-    const event = await Event.create({
-      recipeId: recipe.id,
-      dateOfEvent: Date.now(),
-      notes: 'It went well',
-      rating: 4
-    });
-
     return request(app)
       .get(`/api/v1/events/${event._id}`)
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           // whole recipe here because we populate in route
-          recipeId: {
-            _id: expect.any(String),
-            name: 'cookies',
-            ingredients: [
-              { _id: expect.any(String), name: 'flour', amount: 1, measurement: 'cup' }
-            ],
-            directions: [
-              'preheat oven to 375',
-              'mix ingredients',
-              'put dough on cookie sheet',
-              'bake for 10 minutes'
-            ],
-            __v: 0
-          },
+          recipeId: JSON.parse(JSON.stringify(recipe)),
           dateOfEvent: expect.any(String),
-          notes: 'It went well',
-          rating: 4,
+          notes: 'It was good',
+          rating: 5,
           __v: 0
         });
       });
   });
 
   it('updates an event by id', async() => {
-    const recipe = await Recipe.create({
-      name: 'cookies',
-      ingredients: [
-        { name: 'flour', amount: 1, measurement: 'cup' }
-      ],
-      directions: [
-        'preheat oven to 375',
-        'mix ingredients',
-        'put dough on cookie sheet',
-        'bake for 10 minutes'
-      ],
-    });
-
-    const event = await Event.create({
-      recipeId: recipe.id,
-      dateOfEvent: Date.now(),
-      notes: 'It went well',
-      rating: 4
-    });
-
     return request(app)
       .patch(`/api/v1/events/${event._id}`)
       .send({ rating: 5 })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          recipeId: recipe.id,
+          recipeId: recipe._id.toString(),
           dateOfEvent: expect.any(String),
-          notes: 'It went well',
+          notes: 'It was good',
           rating: 5,
           __v: 0
         });
@@ -195,66 +117,12 @@ describe('event routes', () => {
   });
 
   it.skip('deletes an event by id', async() => {
-    // return Recipe.create({
-    //   name: 'cookies',
-    //   ingredients: [
-    //     { name: 'flour', amount: 1, measurement: 'cup' }
-    //   ],
-    //   directions: [
-    //     'preheat oven to 375',
-    //     'mix ingredients',
-    //     'put dough on cookie sheet',
-    //     'bake for 10 minutes'
-    //   ],
-    // })
-    //   .then(recipe => {
-    //     return Event.create({
-    //       recipeId: recipe.id,
-    //       dateOfEvent: Date.now(),
-    //       notes: 'It went well',
-    //       rating: 4
-    //     });
-    //   })
-    //   .then(event => {
-    //     return request(app)
-    //       .delete(`/api/v1/events/${event._id}`);
-    //   })
-    //   .then(res => {
-    //     expect(res.body).toEqual({
-    //       _id: expect.any(String),
-    //       recipeId: recipe.id,
-    //       dateOfEvent: expect.any(String),
-    //       notes: 'It went well',
-    //       rating: 4,
-    //       __v: 0
-    //     });
-    //   });
-    const recipe = await Recipe.create({
-      name: 'cookies',
-      ingredients: [
-        { name: 'flour', amount: 1, measurement: 'cup' }
-      ],
-      directions: [
-        'preheat oven to 375',
-        'mix ingredients',
-        'put dough on cookie sheet',
-        'bake for 10 minutes'
-      ],
-    });
-
-    const event = await Event.create({
-      recipeId: recipe.id,
-      dateOfEvent: Date.now(),
-      notes: 'It went well',
-      rating: 4
-    });
-
     return request(app)
       .delete(`/api/v1/events/${event._id}`)
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          recipeId: recipe.id,
+          recipeId: recipe._id.toString(),
           dateOfEvent: expect.any(String),
           notes: 'It went well',
           rating: 4,
